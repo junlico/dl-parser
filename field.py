b1b8a9a4ff437aafbf9db530dedc1d89d2d9444d
junlico <sean.junlico@gmail.com>
2020-01-19 15:39:35 -0800

    Skill

--- field.py
+++ field.py
@@ -25,15 +25,42 @@ class Common(ABC):
         self.file_name = file_name
         self.files = [file_name] if files is None else files
 
+    def attach_data(self, attr=None):
+        if attr is None:
+            key = self.file_name
+            data = self.data
+        else:
+            key = attr
+            data = read_json(attr)
+
+        if getattr(self, key, None) is None:
+            setattr(Common, key, data)
+
+    def get_value(self, attr, key):
+        return getattr(self, attr, {}).get(key, None)
+
     def save_file(self):
         save_json(self.file_name, self.data)
 
     def reset_item(self):
         self.data[self.item["Id"]] = self.item
-        self.reset_item()
+        self.item = {}
 
-    def attach_data(self):
-        setattr(Common, self.file_name, self.data)
+    def parse_item(self, f, line, key):
+        r = re.search(r"_(\w+) = \"?(-?\w*)\"?", line)
+
+        if r is None:
+            return
+
+        prop, value = r.groups()
+
+        str_props = getattr(self, "str_props")
+        int_props = getattr(self, "int_props")
+
+        if prop in str_props:
+            self.item[prop] = value
+        elif prop in int_props:
+            self.item[prop] = int(value)
 
     def modify_item(self):
         pass
@@ -87,6 +114,20 @@ class Label(Common):
 
     def finalize(self):
         self.attach_data()
+
+
+class Skill(Common):
+    def __init__(self):
+        super().__init__("skills")
+        self.str_props = ("Id", "Name", "SkillLv1IconName")
+        self.int_props = ()
+        self.attach_data("labels")
+
+    def modify_item(self):
+        if self.item:
+            self.item["Name"] = self.get_value("labels", self.item["Name"])
+            self.item["Icon"] = self.item.pop("SkillLv1IconName")
+            self.reset_item()
 
 
 if __name__ == "__main__":
