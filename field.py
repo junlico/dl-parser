2b369bbf71fe2e1055966a477a69dc6685d7eda6
junlico <sean.junlico@gmail.com>
2020-01-23 20:43:26 -0800

    new structure, class Name and Label

--- field.py
+++ field.py
@@ -1,244 +1,107 @@
 import re
-from abc import ABC
+from abc import ABC, abstractclassmethod
 
-from utils import read_json, save_json
-
-ELEMENT_TYPE = [None, "Flame", "Water", "Wind", "Light", "Shadow"]
+from utils import save_json
 
 
-def parse_label(pattern, f, line):
-    r = re.search(pattern, line)
-
-    if r is None:
-        return
-
-    value = r[1]
-    line = f.readline()
-    r = re.search(r"\"(.+)\"", line)
-
-    if r is not None:
-        return value, r[1]
-
-
-class Common(ABC):
+class Base1(ABC):
     def __init__(self, file_name, files=None):
         self.data = {}
-        self.item = {}
         self.file_name = file_name
         self.files = [file_name] if files is None else files
 
-    def attach_data(self, attr=None):
-        if attr is None:
-            key = self.file_name
-            data = self.data
-        else:
-            key = attr
-            data = read_json(attr)
+    def attach_data(self):
+        setattr(Base1, self.file_name, self.data)
 
-        if getattr(self, key, None) is None:
-            setattr(Common, key, data)
+    def parse_item(self, f, line):
+        pattern = self.get_pattern()
 
-    def get_value(self, attr, key):
-        return getattr(self, attr, {}).get(key, None)
+        if not (k := re.search(pattern, line)):
+            return
 
-    def set_value(self, attr, key, value):
-        data = getattr(self, attr, {})
-        data[key] = value
+        # read next line to get value
+        if v := re.search(r"\"(.+)\"", f.readline()):
+            self.modify_item(k[1], v[1])
 
-    def save_file(self):
+    def finalize(self):
+        self.attach_data()
         save_json(self.file_name, self.data)
 
     def register_item(self):
-        self.data[self.item["Id"]] = self.item
-        self.item = {}
-
-    def parse_item(self, f, line, key):
-        r = re.search(r"_(\w+) = \"?(-?\w*)\"?", line)
-
-        if r is None:
-            return
-
-        prop, value = r.groups()
-
-        str_props = getattr(self, "str_props")
-        int_props = getattr(self, "int_props")
-
-        if prop in str_props:
-            self.item[prop] = value
-        elif prop in int_props:
-            self.item[prop] = int(value)
-
-    def modify_item(self):
         pass
 
-    def finalize(self):
+    def modify_item(self, key, value):
+        pass
+
+    @abstractclassmethod
+    def get_pattern(self):
         pass
 
 
-class Name(Common):
+class Name(Base1):
     def __init__(self):
         super().__init__("names", ["en", "ja", "zh"])
+        self.lang = "en"
 
-    def parse_item(self, f, line, key):
-        pattern = r"Id = \"((?:CHARA|WEAPON|DRAGON|AMULET|FORT_PLANT)_NAME_\d+)\""
-
-        r = parse_label(pattern, f, line)
-
-        if r is None:
-            return
-
-        uid, value = r
-        item = self.data.get(uid, {"en": "", "ja": "", "zh": ""})
-        item[key] = value
-        self.data[uid] = item
+    def modify_item(self, key, value):
+        item = self.data.get(key, {"en": "", "ja": "", "zh": ""})
+        item[self.lang] = value
+        self.data[key] = item
 
     def finalize(self):
-        self.data["CHARA_NAME_10150403"] = {
-            "en": "Euden",
-            "ja": "ユーディル",
-            "zh": "尤蒂尔",
-        }
+        self.data["CHARA_NAME_10150403"] = self.data["CHARA_NAME_10140101"]
+        super().finalize()
 
-        self.attach_data()
+    def get_pattern(self):
+        return r"Id = \"((?:CHARA|WEAPON|DRAGON|AMULET|FORT_PLANT)_NAME_\d+)\""
 
 
-class Label(Common):
+class Label(Base1):
     def __init__(self):
         super().__init__("labels", ["en"])
 
-    def parse_item(self, f, line, key):
-        pattern = r"Id = \"((?:ABILITY_NAME|SKILL_NAME|ABILITY_DETAIL)_\d+)\""
-        r = parse_label(pattern, f, line)
-
-        if r is None:
-            return
-
-        uid, value = r
+    def modify_item(self, key, value):
         value = value.replace(" \\n", " ")
         value = value.replace("\\n", " ")
-        self.data[uid] = value
+        self.data[key] = value
+
+    def get_pattern(self):
+        return r"Id = \"((?:ABILITY_NAME|SKILL_NAME|ABILITY_DETAIL)_\d+)\""
+
+
+class Base2(Base1):
+    def __init__(self, file_name):
+        super().__init__(file_name)
+        self.item = {}
+
+    def parse_item(self):
+        print("Base2 parse_item")
 
     def finalize(self):
-        self.attach_data()
+        save_json(self.finalize, self.data)
+
+    def get_pattern(self):
+        return r"_(\w+) = \"?(-?\w*)\"?"
 
 
-class Skill(Common):
+class Adventurer(Base2):
     def __init__(self):
-        super().__init__("skills")
-        self.str_props = ("Id", "Name", "SkillLv1IconName")
-        self.int_props = ()
-        self.attach_data("labels")
-
-    def modify_item(self):
-        if self.item:
-            self.item["Name"] = self.get_value("labels", self.item["Name"])
-            self.item["Icon"] = self.item.pop("SkillLv1IconName")
-            self.register_item()
+        super().__init__("adventurer")
 
 
-class Ability(Common):
+class Weapon(Base2):
     def __init__(self):
-        super().__init__("abilities")
-        self.str_props = (
-            "Id",
-            "Name",
-            "Details",
-            "AbilityIconName",
-            "AbilityType1",
-            "AbilityType2",
-            "AbilityType3",
-            "VariousId1a",
-            "VariousId2a",
-            "VariousId3a",
-        )
-        self.int_props = (
-            "PartyPowerWeight",
-            "ElementalType",
-            "ConditionType",
-            "ConditionValue",
-            "AbilityType1UpValue",
-            "AbilityType2UpValue",
-            "AbilityType3UpValue",
-        )
-        self.__enemy = {
-            "3": "hms",
-            "5": "hmc",
-            "7": "hbh",
-            "9": "hjp",
-            "11": "hzd",
-        }
+        super().__init__("weapon")
 
-        self.attach_data("labels")
-        self.attach_data("ability_val")
-        self.save_ability_val = False
 
-    def modify_item(self):
-        item = self.item
+class Dragon(Base2):
+    def __init__(self):
+        super().__init__("dragon")
 
-        if not item:
-            return
 
-        if not self.get_value("labels", item["Name"]):
-            self.item = {}
-            return
-
-        for k in ["Name", "Details"]:
-            text = self.get_value("labels", item[k])
-
-            if text is None:
-                continue
-
-            val = self.__get_val(text)
-            element = self.__get_element(text)
-
-            text = text.format(
-                ability_shift0="",
-                ability_val0=val,
-                element_owner=element,
-                ability_cond0=item["ConditionValue"],
-            )
-
-            item[k] = text
-
-        del item["ConditionValue"]
-        self.register_item()
-
-    def finalize(self):
-        if self.save_ability_val:
-            save_json("ability_val", getattr(self, "ability_val"))
-
-    def __get_val(self, text):
-        item = self.item
-        if item["AbilityType1UpValue"] == 0 and "{ability_val0}" in text:
-            uid = item["Id"]
-            if not (val := self.get_value("ability_val", uid)):
-                print("{}: ability_val0 loss".format(uid))
-                self.set_value("ability_val", uid, "")
-                self.save_ability_val = True
-            else:
-                item["AbilityType1UpValue"] = val
-
-        return item["AbilityType1UpValue"]
-
-    def __get_element(self, text):
-        item = self.item
-        element = None
-        if "{element_owner}" in text:
-            if item["ElementalType"] == 0:
-                uid = item["Id"]
-                r = re.search(r"(Flame|Water|Wind|Light|Shadow)", item["Name"])
-                if r is not None:
-                    element = r[1]
-                elif not (element := self.get_value("ability_val", uid)):
-                    print("{}: element_owner loss".format(uid))
-                    self.set_value("ability_val", uid, "")
-                    self.save_ability_val = True
-
-                item["ElementalType"] = ELEMENT_TYPE.index(element)
-            else:
-                element = ELEMENT_TYPE[item["ElementalType"]]
-
-        return element
+class Wyrmprint(Base2):
+    def __init__(self):
+        super().__init__("wyrmprint")
 
 
 if __name__ == "__main__":
