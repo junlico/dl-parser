bb5c6ed2e75b255b8e7d3c7f3df662572e417851
junlico <sean.junlico@gmail.com>
2020-01-23 20:40:44 -0800

    method to extract files from zip

--- utils.py
+++ utils.py
@@ -1,27 +1,64 @@
 import json
+import re
+import zipfile
 from pathlib import Path
+from zipfile import ZipFile, is_zipfile
 
-
-ROOT_PATH = Path("/home/jl/Documents/dragalia-lost")
+APP_PATH = Path("/home/jl/Documents/dragalia-lost")
+DOWNLOAD_PATH = Path("/home/jl/Downloads")
 SCRIPT_PATH = Path("/home/jl/Documents/script")
-EXPORTS = "z_files"
+DECIPHER_PATH = SCRIPT_PATH / "DecipherFiles"
+EXPORTS = "Exports"
 
 
-def mono_file(file_name):
+def update_text_file():
+    def extract_file(z, f):
+        z.extract(f, DECIPHER_PATH)
+        print("Update file: {}/{}".format(DECIPHER_PATH, f.filename))
+
+    # update EN files
+    en_zip_name = input("EN Zip Name in Downloads: ")
+    en_zip_path = DOWNLOAD_PATH / "{}.zip".format(en_zip_name)
+
+    if not is_zipfile(en_zip_path):
+        raise Exception("EN Zip: Wrong path or not exists.")
+
     files = {
-        "en": "TextLabel",
-        "ja": "TextLabelJP",
-        "zh": "TextLabelZH",
-        "abilities": "AbilityData",
-        "skills": "SkillData",
-        "adventurer": "CharaData",
-        "weapon": "WeaponData",
-        "dragon": "DragonData",
-        "wyrmprint": "AmuletData",
-        "facility": "FortPlantDetail",
+        "AbilityData": "abilities",
+        "AmuletData": "wyrmprint",
+        "CharaData": "adventurer",
+        "DragonData": "dragon",
+        "FortPlantDetail": "facility",
+        "SkillData": "skills",
+        "TextLabel": "en",
+        "TextLabelJP": "ja",
+        "WeaponData": "weapon",
     }
 
-    return SCRIPT_PATH / "MonoBehaviour" / "{}.txt".format(files[file_name])
+    # Make sure MonoBehaviour folder in zip files
+    pattern = r"MonoBehaviour/({}).txt".format("|".join(files.keys()))
+    with ZipFile(en_zip_path) as z:
+        for f in z.infolist():
+            if r := re.search(pattern, f.filename):
+                f.filename = "{}.txt".format(files[r[1]])
+                extract_file(z, f)
+
+    # update ZH TextLabel
+    zh_zip_path = DOWNLOAD_PATH / "qq-files" / "2236526965" / "file_recv" / "master.zip"
+
+    if not is_zipfile(zh_zip_path):
+        raise Exception("ZH Zip: Wrong path or not exists.")
+
+    with ZipFile(zh_zip_path) as z:
+        for f in z.infolist():
+            if f.filename == "master/TextLabel.txt":
+                f.filename = "zh.txt"
+                extract_file(z, f)
+                break
+
+
+def get_text_path(file_name):
+    return DECIPHER_PATH / "{}.txt".format(file_name)
 
 
 def get_json_path(folder, file_name):
@@ -44,8 +81,9 @@ def save_json(file_name, data, folder=EXPORTS):
     with path.open("w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
         f.write("\n")
-    print("save file: {}".format(path))
+    print("Save file: {}".format(path))
 
 
 if __name__ == "__main__":
+    update_text_file()
     pass
